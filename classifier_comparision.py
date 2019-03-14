c294abe9dbfc52e06954863b8e8d6208814d6a01
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2019-03-13 21:40:51 -0500

    Added right angle triangle dataset classification

--- classifier_comparision.py
+++ classifier_comparision.py
@@ -19,11 +19,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-
+# Classifier related constants
 NAMES = [
-    "Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-    "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-    "Naive Bayes", "QDA"
+    "Nearest Neighbors",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Random Forest",
+    "Neural Net",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA"
 ]
 
 CLASSIFIERS = [
@@ -33,37 +40,82 @@ CLASSIFIERS = [
     GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    MLPClassifier(alpha=1),
+    MLPClassifier(
+        activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
+        beta_2=0.999, early_stopping=False, epsilon=1e-08,
+        hidden_layer_sizes=(30, 30, 30), learning_rate='constant',
+        learning_rate_init=0.001, max_iter=200, momentum=0.9,
+        nesterovs_momentum=True, power_t=0.5, random_state=None,
+        shuffle=True, solver='adam', tol=0.0001, validation_fraction=0.1,
+        verbose=False, warm_start=False
+    ),
     AdaBoostClassifier(),
     GaussianNB(),
     QuadraticDiscriminantAnalysis()
 ]
 
+
+# File related constants
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
-TRAINING_FILE = os.path.join(APP_DIR, "training.csv")
-DATASET = pnd.read_csv(TRAINING_FILE)
+DATA_DIR = os.path.join(APP_DIR, 'data')
+TRIANGLE_FILE = os.path.join(DATA_DIR, "triangle.csv")
+DATASET1_FILE = os.path.join(DATA_DIR, "dataset1.csv")
+DATASET2_FILE = os.path.join(DATA_DIR, "dataset2.csv")
+DATASET3_FILE = os.path.join(DATA_DIR, "dataset3.csv")
+DATASET_FILES = [
+    TRIANGLE_FILE, DATASET1_FILE, DATASET2_FILE, DATASET3_FILE
+]
+TESTING_DATASET_NAMES = [
+    "Normal Triangle",
+    "Right Triangle Dataset 1",
+    "Right Triangle Dataset 2",
+    "Right Triangle Dataset 3"
+]
+TESTING_DATASET = []
 
 
 def main():
     """Main function."""
-    X_train, X_test, y_train, y_test = train_test_split(
-        DATASET.loc[:, 'side1':'side3'],
-        DATASET.loc[:, 'validity'],
-        test_size=0.20,
-        train_size=0.80,
-        random_state=269380
-    )
+
+    for filename in DATASET_FILES:
+        TESTING_DATASET.append(pnd.read_csv(filename))
 
     for name, classifier in zip(NAMES, CLASSIFIERS):
-        print("-" * 64)
+        print("-" * 40)
         print(name + " Classification Result")
-        print("-" * 64)
+        print("-" * 40)
 
-        classifier.fit(X_train, y_train)
-        score = classifier.score(X_test, y_test)
+        for i in range(len(TESTING_DATASET)):
+            if i == 0 or i == 1:
+                X_train, X_test, y_train, y_test = train_test_split(
+                    TESTING_DATASET[i].loc[:, 'side1':'side3'],
+                    TESTING_DATASET[i].loc[:, 'validity'],
+                    test_size=0.20,
+                    train_size=0.80,
+                    random_state=345123
+                )
+            if i == 2:
+                X_train, X_test, y_train, y_test = train_test_split(
+                    TESTING_DATASET[i].loc[:, 'side1':'side32'],
+                    TESTING_DATASET[i].loc[:, 'validity'],
+                    test_size=0.20,
+                    train_size=0.80,
+                    random_state=269380
+                )
+            else:
+                X_train, X_test, y_train, y_test = train_test_split(
+                    TESTING_DATASET[i].loc[:, 'side12':'side32'],
+                    TESTING_DATASET[i].loc[:, 'validity'],
+                    test_size=0.20,
+                    train_size=0.80,
+                    random_state=7887631
+                )
 
-        print('%.2f' % (score))
-        print("-" * 64 + "\n")
+            classifier.fit(X_train, y_train)
+            score = classifier.score(X_test, y_test)
+            print(TESTING_DATASET_NAMES[i] + ': %.2f' % (score))
+
+        print("-" * 40 + "\n")
 
 
 if __name__ == "__main__":
