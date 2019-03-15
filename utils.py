5b27c612d856f66715d4bcb432a60a0ab2084514
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2019-03-15 17:31:13 -0500

    Changed the labels where square is respresented as ^2

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import os
 # File related constants
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(APP_DIR, 'data')
+IMAGE_DIR = os.path.join(APP_DIR, 'image')
 
 T_DATASET1 = os.path.join(DATA_DIR, "t_dataset1.csv")
 T_DATASET2 = os.path.join(DATA_DIR, "t_dataset2.csv")
@@ -22,6 +23,16 @@ RT_FILE_NAMES = [
     RT_DATASET1, RT_DATASET2, RT_DATASET3, RT_DATASET4, RT_DATASET5
 ]
 
+# Image file constants for normal triangles
+T_IMAGE1 = os.path.join(IMAGE_DIR, "t_image1.png")
+T_IMAGE2 = os.path.join(IMAGE_DIR, "t_image2.png")
+
+# Image file constants for right angle triangles
+RT_IMAGE1 = os.path.join(IMAGE_DIR, "rt_image1.png")
+RT_IMAGE2 = os.path.join(IMAGE_DIR, "rt_image2.png")
+RT_IMAGE3 = os.path.join(IMAGE_DIR, "rt_image3.png")
+IMAGE_FILES = [T_IMAGE1, T_IMAGE2, RT_IMAGE1, RT_IMAGE2, RT_IMAGE3]
+
 # Normal triangle dataset related constants
 T_LABEL1 = ['side1', 'side2', 'side3', 'validity']
 T_LABEL2 = [
@@ -33,8 +44,8 @@ T_LABELS = [T_LABEL1, T_LABEL2, T_LABEL3]
 
 # Right triangle related constants
 RT_LABEL1 = ['side1', 'side2', 'side3', 'validity']
-RT_LABEL2 = ['side1', 'side2', 'side3', 'side12', 'side22', 'side32', 'validity']
-RT_LABEL3 = ['side12', 'side22', 'side32', 'validity']
+RT_LABEL2 = ['side1', 'side2', 'side3', 'side1^2', 'side2^2', 'side3^2', 'validity']
+RT_LABEL3 = ['side1^2', 'side2^2', 'side3^2', 'validity']
 RT_LABEL4 = ['side1+side2', 'side1+side3', 'side2+side3', 'validity']
-RT_LABEL5 = ['side12+side22', 'side12+side32', 'side22+side32', 'validity']
+RT_LABEL5 = ['side1^2+side2^2', 'side1^2+side3^2', 'side2^2+side3^2', 'validity']
 RT_LABELS = [RT_LABEL1, RT_LABEL2, RT_LABEL3, RT_LABEL4, RT_LABEL5]
